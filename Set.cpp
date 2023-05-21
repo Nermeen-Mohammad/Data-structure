7a1eeab248b06e9b5e1ab4977d9a8d9017d9e8f6
Nermeen Almomani <nermeenalmomani4@gmail.com>
2023-05-21 22:54:14 +0300

    set

---
+++ Set.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include<set>
+using namespace std;
+int main() {
+    
+    //set<int> set1{1,9,4,0,2};
+    //for(auto elements: set1)
+    //cout << elements <<endl;
+    
+  //  set1.insert(19); 
+   // auto it = set1.begin();
+   // set1.insert(it, 19); // fast insert cause the (it) like a hint
+   // return 0;
+   
+   //set<string> set2;
+   //auto it = set2.emplace("Nermeen");
+   //cout <<*it.first;
+   
+  // set1.erase(2);
+  //    for(auto elements: set1)
+  //    cout << elements <<endl;
+  
+  
+  //set<int> set3{1,3,9,6,4,3};
+  //auto it = set3.begin();
+  //++++++it;
+  //set3.erase(it);  
+  //for(auto elements: set3) {
+  //  cout << elements <<endl;
+  //}
+  
+// set <int> set4 {10,20,30,40,50,60};
+// auto it = set4.find(30);
+// set4.erase(it, set4.end());
+// for (auto x : set4)
+// cout << x<< " ";
+ 
+// auto it1 = set4.lower_bound(20);
+// auto it2 = set4.upper_bound(50);
+//  set4.erase(it1,it2);
+//  for (auto x : set4)
+// cout << x<< " ";
+
+//set <int> s1{3,2,1,6,7};
+//set <int> s2{10,30,70};
+//s1.swap(s2);
+//
+//for (auto x : s1) {
+//cout << x<< " "; }
+//cout<<endl;
+//for (auto y : s2) {
+//cout << y<< " "; 
+//
+//}
+
+
+//set<int> ss{10,59,40,30,48,92};
+//auto com = ss.key_comp();
+//auto it = ss.begin();
+//int c = 40;
+//for (; com(*it, c); it++) {
+//    cout << *it << endl;
+//    
+//    }
+
+
+//cout << ss.count(59) <<endl;
+//cout << ss.count(1);
+
+//auto it =ss.equal_range(40);
+//cout << *it.first << endl;
+//cout << *it.second << endl;
+
+//set<int, greater<int>> sk{10,43,65,82,0};
+//auto it = sk.begin();
+//for (auto y : sk) 
+//cout << y<< " ";
+
+//multiset<int> x {1,1,5,1,8};
+//cout << x.count(1);
+
+
+}
